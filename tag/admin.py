a50cfaed879c7a7de7f18fd9694209a1477d364e
gu-ss-tavo <gusstavo.jarabs@gmail.com>
2022-01-27 02:23:42 +0000

    Tag added

--- tag/admin.py
+++ tag/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from tag.models import Tag
+
 # Register your models here.
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {
+        'slug': ('name',)
+    }
--- tag/models.py
+++ tag/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+    slug = models.SlugField(max_length=50, unique='name')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
